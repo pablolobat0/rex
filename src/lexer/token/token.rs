77177f0cff59f89b07e3c1f0f68c2241c886abfc
pablolobat0 <plobatorey@gmail.com>
2024-07-04 12:45:24 +0200

    format(token): Add display format for TokenType

--- src/lexer/token/token.rs
+++ src/lexer/token/token.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum TokenType {
@@ -44,6 +45,50 @@ pub enum TokenType {
     EOF,
     // Illegal
     Illegal,
+}
+
+impl fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let token_str = match self {
+            TokenType::LeftParen => "(",
+            TokenType::RightParen => ")",
+            TokenType::LeftBrace => "{",
+            TokenType::RightBrace => "}",
+            TokenType::Illegal => "Illegal",
+            TokenType::EOF => "EOF",
+            TokenType::Identifier => "Identifier",
+            TokenType::String => "String",
+            TokenType::Integer => "Integer",
+            TokenType::Plus => "Plus",
+            TokenType::Minus => "Minus",
+            TokenType::Star => "Star",
+            TokenType::Slash => "Slash",
+            TokenType::Equal => "Equal",
+            TokenType::Less => "Less",
+            TokenType::Greater => "Greater",
+            TokenType::Colon => "Colon",
+            TokenType::Semicolon => "Semicolon",
+            TokenType::Comma => "Comma",
+            TokenType::Dot => "Dot",
+            TokenType::Question => "Question",
+            TokenType::Bang => "Bang",
+            TokenType::BangEqual => "BangEqual",
+            TokenType::EqualEqual => "EqualEqual",
+            TokenType::GreaterEqual => "GreaterEqual",
+            TokenType::LessEqual => "LessEqual",
+            TokenType::And => "And",
+            TokenType::Or => "Or",
+            TokenType::Let => "Let",
+            TokenType::Function => "Function",
+            TokenType::Return => "Return",
+            TokenType::True => "True",
+            TokenType::False => "False",
+            TokenType::If => "If",
+            TokenType::Else => "Else",
+            TokenType::Null => "Null",
+        };
+        write!(f, "{}", token_str)
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
