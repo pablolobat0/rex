cffeb603aae4238f58a2f1e354496c309852fe33
pablolobat0 <plobatorey@gmail.com>
2024-07-12 09:14:08 +0200

    feat(evaluator): implement prefix operators

--- src/evaluator/evaluator.rs
+++ src/evaluator/evaluator.rs
@@ -1,6 +1,9 @@
+use super::object::object::Object;
 use crate::parser::ast::ast::{Expression, Node, Statement};
 
-use super::object::object::Object;
+const TRUE: Object = Object::Boolean(true);
+const FALSE: Object = Object::Boolean(false);
+const NULL: Object = Object::Null;
 
 pub fn eval(node: Node) -> Object {
     match node {
@@ -11,7 +14,7 @@ pub fn eval(node: Node) -> Object {
 }
 
 fn eval_statements(statements: Vec<Statement>) -> Object {
-    let mut result = Object::Null;
+    let mut result = NULL;
     for statement in statements {
         result = eval(Node::Statement(statement));
     }
@@ -21,15 +24,49 @@ fn eval_statements(statements: Vec<Statement>) -> Object {
 fn eval_expression(expression: Expression) -> Object {
     match expression {
         Expression::Integer(integer) => Object::Integer(integer.value),
-        Expression::Boolean(boolean) => Object::Boolean(boolean.value),
+        Expression::Boolean(boolean) => eval_boolean(boolean.value),
+        Expression::Prefix(prefix_expression) => {
+            let right = eval(Node::Expression(*prefix_expression.right));
+            return eval_prefix_expression(&prefix_expression.operator, right);
+        }
         _ => todo!(),
+    }
+}
+
+fn eval_boolean(boolean: bool) -> Object {
+    if boolean {
+        TRUE
+    } else {
+        FALSE
+    }
+}
+
+fn eval_prefix_expression(operator: &str, right: Object) -> Object {
+    match operator {
+        "!" => eval_bang_operator(right),
+        "-" => eval_minus_prefix_operator(right),
+        _ => todo!(),
+    }
+}
+
+fn eval_bang_operator(object: Object) -> Object {
+    match object {
+        Object::Boolean(boolean) => eval_boolean(!boolean),
+        _ => NULL,
+    }
+}
+
+fn eval_minus_prefix_operator(object: Object) -> Object {
+    match object {
+        Object::Integer(value) => Object::Integer(-value),
+        _ => NULL,
     }
 }
 
 fn eval_statement(statement: Statement) -> Object {
     match statement {
         Statement::Expression(expression_statement) => {
-            eval(Node::Expression(expression_statement.expression))
+            return eval(Node::Expression(expression_statement.expression))
         }
         _ => todo!(),
     }
